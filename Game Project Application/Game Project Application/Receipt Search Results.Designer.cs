a42bef477c90040bb06adc9e6130eca3daa143b3
apjavier <apjavier@ksu.edu>
2019-04-19 16:55:03 -0500

    merging

--- Game Project Application/Game Project Application/Receipt Search Results.Designer.cs
+++ Game Project Application/Game Project Application/Receipt Search Results.Designer.cs
@@ -28,26 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.uxResults = new System.Windows.Forms.DataGridView();
             this.ReceiptId = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.FirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.LastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Email = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.GameCount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Total = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.uxResults)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // uxResults
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.uxResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.uxResults.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.ReceiptId,
             this.FirstName,
             this.LastName,
-            this.Email});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(559, 432);
-            this.dataGridView1.TabIndex = 0;
+            this.Email,
+            this.GameCount,
+            this.Total});
+            this.uxResults.Location = new System.Drawing.Point(12, 12);
+            this.uxResults.Name = "uxResults";
+            this.uxResults.Size = new System.Drawing.Size(559, 432);
+            this.uxResults.TabIndex = 0;
             // 
             // ReceiptId
             // 
@@ -70,25 +74,37 @@
             this.Email.HeaderText = "Email";
             this.Email.Name = "Email";
             // 
+            // GameCount
+            // 
+            this.GameCount.HeaderText = "Games Purchased";
+            this.GameCount.Name = "GameCount";
+            // 
+            // Total
+            // 
+            this.Total.HeaderText = "Total";
+            this.Total.Name = "Total";
+            // 
             // ReceiptSearchResults
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(583, 456);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.uxResults);
             this.Name = "ReceiptSearchResults";
             this.Text = "Receipt Search Results";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uxResults)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView uxResults;
         private System.Windows.Forms.DataGridViewTextBoxColumn ReceiptId;
         private System.Windows.Forms.DataGridViewTextBoxColumn FirstName;
         private System.Windows.Forms.DataGridViewTextBoxColumn LastName;
         private System.Windows.Forms.DataGridViewTextBoxColumn Email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GameCount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Total;
     }
 }
