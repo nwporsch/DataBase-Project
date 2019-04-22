dfe9509344edc122e71399f7a9644ad6016bda9b
Nickalas Porsch <porschnickalas@gmail.com>
2019-04-22 12:24:02 -0500

    Creating new procedure.

---
+++ Game Project Application/Game Project Application/SQL/Procedures/GetStoreInfo.sql
@@ -0,0 +1,10 @@
+﻿--CREATE OR ALTER PROCEDURE GameStore.GetCustomerId
+--	@StoreId INT
+--AS
+
+SELECT S.Address, S.City, S.State, H.StartingTime, H.EndingTime, H.Description, SUM(GSI.Quantity) AS GameCount,  
+FROM GameStore.Stores S
+INNER JOIN GameStore.Hours H ON H.StoreId = S.StoreId
+INNER JOIN GameStore.GameStoreInfo GSI ON GSI.StoreId = S.StoreId
+INNER JOIN GameStore.OrderLines OL ON OL.
+
