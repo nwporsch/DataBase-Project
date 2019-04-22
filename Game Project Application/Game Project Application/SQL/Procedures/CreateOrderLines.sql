8c1981824e92d1f57263eda4d1a18dd8b7f45280
Nickalas Porsch <porschnickalas@gmail.com>
2019-04-22 14:37:38 -0500

    I think I finished the CreateOrderLines sql file

--- Game Project Application/Game Project Application/SQL/Procedures/CreateOrderLines.sql
+++ Game Project Application/Game Project Application/SQL/Procedures/CreateOrderLines.sql
@@ -5,35 +5,18 @@
 	@UnitPrice AS DECIMAL(4,2)
 AS
 
-MERGE Games
-INSERT INTO GameStore.OrderLines(OrderId,GameStoreInfoId,UnitPrice,Quantity)
-	VALUES(@OrderId,@GameStoreInfoId,@UnitPrice,@Quantity);
-GO
-
-
-CREATE OR ALTER PROCEDURE GameStore.CreateOrderLines
-	@OrderId AS INT,
-	@GameStoreInfoId AS INT,
-	@Quantity AS INT,
-	@UnitPrice AS DECIMAL(4,2)
-AS
-
-SELECT *
-FROM GameStore.OrderLines OL;
-
 MERGE GameStore.OrderLines OL
 	USING(
 		SELECT T.OrderId, T.GameStoreInfoId, T.Quantity, T.UnitPrice 
 		FROM(
-			VALUES (1,1,1,1)
+			VALUES (@OrderId,@GameStoreInfoId,@Quantity,@UnitPrice)
 			)
 		 T (OrderId,GameStoreInfoId,Quantity,UnitPrice)
 	)TH(OrderId,GameStoreInfoId,Quantity,UnitPrice) ON TH.OrderId = OL.OrderId
 WHEN MATCHED THEN
 	UPDATE
 	SET
-		Quantity = Quantity + 1;
+		Quantity = OL.Quantity + TH.Quantity
 WHEN NOT MATCHED THEN
-	INSERT INTO GameStore.OrderLines(OrderId,GameStoreInfoId,UnitPrice,Quantity)
-	VALUES(1,1,1,1)
-END;
+	INSERT (OrderId,GameStoreInfoId,UnitPrice,Quantity)
+		VALUES(@OrderId,@GameStoreInfoId,@UnitPrice,@Quantity);
