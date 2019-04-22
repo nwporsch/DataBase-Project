26851064e82c40455740fbd34a5db462a9ffa38e
Nickalas Porsch <porschnickalas@gmail.com>
2019-04-22 11:04:18 -0500

    Fixed the OrderLines table

--- Game Project Application/Game Project Application/SQL/Tables/GameStore.OrderLines.sql
+++ Game Project Application/Game Project Application/SQL/Tables/GameStore.OrderLines.sql
@@ -2,11 +2,12 @@
 BEGIN
    CREATE TABLE GameStore.OrderLines
    (
+	  OrderLineId INT IDENTITY(1,1) NOT NULL,
       OrderId INT FOREIGN KEY REFERENCES GameStore.Orders(OrderId )NOT NULL,
 	  GameId INT FOREIGN KEY REFERENCES GameStore.Games(GameId) NOT NULL,
 	  Quantity INT CHECK(Quantity >= 0) NOT NULL,
 	  UnitPrice DECIMAL(4,2) CHECK(UnitPrice > 0) NOT NULL
 
-	  PRIMARY KEY(OrderId, GameId)
+	  PRIMARY KEY(OrderId, OrderLineId)
    );
 END;
