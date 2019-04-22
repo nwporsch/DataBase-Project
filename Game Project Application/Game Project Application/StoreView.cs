f83c7e3e574ff2080ac34679fb1f66bb03c826be
apjavier <apjavier@ksu.edu>
2019-04-22 14:17:06 -0500

    StoreView, implemented btnSales_Click and added a get SalesInfo method that returns the sales and number of orders from a given month or year.

--- Game Project Application/Game Project Application/StoreView.cs
+++ Game Project Application/Game Project Application/StoreView.cs
@@ -178,12 +178,218 @@ namespace Game_Project_Application
             }
         }
 
+        /// <summary>
+        /// Returns the total number of orders made and total sales of month to date and year to date
+        /// </summary>
+        /// <param name="storeId"></param>
+        /// <returns></returns>
+        private string[] getSalesInfo(int storeId, int month)
+        {
+            string connectionString = "Server=mssql.cs.ksu.edu;Database=cis560_team21; Integrated Security=true";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var command = new SqlCommand("GameStore.GetSalesInfo", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("StoreId", storeId);
+                    connection.Open();
+
+                    var k = command.ExecuteReader();
+
+                    string totalSales = "";
+                    string orders = "";
+
+                    while (k.Read())
+                    {
+                        switch (month)
+                        {
+                            case 1:
+                                if (k.GetString(k.GetOrdinal("Month")).Equals("January"))
+                                {
+                                    orders = k.GetInt32(k.GetOrdinal("Orders")).ToString();
+                                    totalSales = k.GetDecimal(k.GetOrdinal("TotalSales")).ToString();
+                                }
+                                break;
+                            case 2:
+                                if (k.GetString(k.GetOrdinal("Month")).Equals("February"))
+                                {
+                                    orders = k.GetInt32(k.GetOrdinal("Orders")).ToString();
+                                    totalSales = k.GetDecimal(k.GetOrdinal("TotalSales")).ToString();
+                                }
+                                break;
+                            case 3:
+                                if (k.GetString(k.GetOrdinal("Month")).Equals("March"))
+                                {
+                                    orders = k.GetInt32(k.GetOrdinal("Orders")).ToString();
+                                    totalSales = k.GetDecimal(k.GetOrdinal("TotalSales")).ToString();
+                                }
+                                break;
+                            case 4:
+                                if (k.GetString(k.GetOrdinal("Month")).Equals("April"))
+                                {
+                                    orders = k.GetInt32(k.GetOrdinal("Orders")).ToString();
+                                    totalSales = k.GetDecimal(k.GetOrdinal("TotalSales")).ToString();
+                                }
+                                break;
+                            case 5:
+                                if (k.GetString(k.GetOrdinal("Month")).Equals("May"))
+                                {
+                                    orders = k.GetInt32(k.GetOrdinal("Orders")).ToString();
+                                    totalSales = k.GetDecimal(k.GetOrdinal("TotalSales")).ToString();
+                                }
+                                break;
+                            case 6:
+                                if (k.GetString(k.GetOrdinal("Month")).Equals("June"))
+                                {
+                                    orders = k.GetInt32(k.GetOrdinal("Orders")).ToString();
+                                    totalSales = k.GetDecimal(k.GetOrdinal("TotalSales")).ToString();
+                                }
+                                break;
+                            case 7:
+                                if (k.GetString(k.GetOrdinal("Month")).Equals("July"))
+                                {
+                                    orders = k.GetInt32(k.GetOrdinal("Orders")).ToString();
+                                    totalSales = k.GetDecimal(k.GetOrdinal("TotalSales")).ToString();
+                                }
+                                break;
+                            case 8:
+                                if (k.GetString(k.GetOrdinal("Month")).Equals("August"))
+                                {
+                                    orders = k.GetInt32(k.GetOrdinal("Orders")).ToString();
+                                    totalSales = k.GetDecimal(k.GetOrdinal("TotalSales")).ToString();
+                                }
+                                break;
+                            case 9:
+                                if (k.GetString(k.GetOrdinal("Month")).Equals("September"))
+                                {
+                                    orders = k.GetInt32(k.GetOrdinal("Orders")).ToString();
+                                    totalSales = k.GetDecimal(k.GetOrdinal("TotalSales")).ToString();
+                                }
+                                break;
+                            case 10:
+                                if (k.GetString(k.GetOrdinal("Month")).Equals("October"))
+                                {
+                                    orders = k.GetInt32(k.GetOrdinal("Orders")).ToString();
+                                    totalSales = k.GetDecimal(k.GetOrdinal("TotalSales")).ToString();
+                                }
+                                break;
+                            case 11:
+                                if (k.GetString(k.GetOrdinal("Month")).Equals("November"))
+                                {
+                                    orders = k.GetInt32(k.GetOrdinal("Orders")).ToString();
+                                    totalSales = k.GetDecimal(k.GetOrdinal("TotalSales")).ToString();
+                                }
+                                break;
+                            case 12:
+                                if (k.GetString(k.GetOrdinal("Month")).Equals("December"))
+                                {
+                                    orders = k.GetInt32(k.GetOrdinal("Orders")).ToString();
+                                    totalSales = k.GetDecimal(k.GetOrdinal("TotalSales")).ToString();
+                                }
+                                break;
+                            default:
+                                if (k.GetString(k.GetOrdinal("Year")).Equals("2019"))
+                                {
+                                    orders = k.GetInt32(k.GetOrdinal("Orders")).ToString();
+                                    totalSales = k.GetDecimal(k.GetOrdinal("TotalSales")).ToString();
+                                }
+                                break;
+                        }
+
+                    }
+
+                    k.Close();
+
+                    string[] output = { orders, totalSales };
+                    return output;
+                }
+            }
+        }
+
 
         private void btnSales_Click(object sender, EventArgs e)
         {
             if (uxStoreId.Text != "")
             {
                 int storeId = Convert.ToInt32(uxStoreId.Text);
+                StringBuilder s = new StringBuilder();
+
+                string[] output;
+
+                for(int i = 1; i <= 12; i++)
+                {
+                    output = getSalesInfo(storeId, i);
+                    switch (i)
+                    {
+                        case 1:
+                            s.Append("Month: January\n" +
+                                     "Monthly Orders: " + output[0] + "\n" +
+                                     "Monthly Sales:  " + output[1] + "\n");
+                            break;
+                        case 2:
+                            s.Append("Month: February\n" +
+                                     "Monthly Orders: " + output[0] + "\n" +
+                                     "Monthly Sales:  " + output[1] + "\n");
+                            break;
+                        case 3:
+                            s.Append("Month: March\n" +
+                                     "Monthly Orders: " + output[0] + "\n" +
+                                     "Monthly Sales:  " + output[1] + "\n");
+                            break;
+                        case 4:
+                            s.Append("Month: April\n" +
+                                     "Monthly Orders: " + output[0] + "\n" +
+                                     "Monthly Sales:  " + output[1] + "\n");
+                            break;
+                        case 5:
+                            s.Append("Month: May\n" +
+                                     "Monthly Orders: " + output[0] + "\n" +
+                                     "Monthly Sales:  " + output[1] + "\n");
+                            break;
+                        case 6:
+                            s.Append("Month: June\n" +
+                                     "Monthly Orders: " + output[0] + "\n" +
+                                     "Monthly Sales:  " + output[1] + "\n");
+                            break;
+                        case 7:
+                            s.Append("Month: July\n" +
+                                     "Monthly Orders: " + output[0] + "\n" +
+                                     "Monthly Sales:  " + output[1] + "\n");
+                            break;
+                        case 8:
+                            s.Append("Month: August\n" +
+                                     "Monthly Orders: " + output[0] + "\n" +
+                                     "Monthly Sales:  " + output[1] + "\n");        
+                            break;
+                        case 9:
+                            s.Append("Month: September\n" +
+                                     "Monthly Orders: " + output[0] + "\n" +
+                                     "Monthly Sales:  " + output[1] + "\n");
+                            break;
+                        case 10:
+                            s.Append("Month: October\n" +
+                                     "Monthly Orders: " + output[0] + "\n" +
+                                     "Monthly Sales:  " + output[1] + "\n");
+                            break;
+                        case 11:
+                            s.Append("Month: November\n" +
+                                     "Monthly Orders: " + output[0] + "\n" +
+                                     "Monthly Sales:  " + output[1] + "\n");
+                            break;
+                        case 12:
+                            s.Append("Month: December\n" +
+                                     "Monthly Orders: " + output[0] + "\n" +
+                                     "Monthly Sales:  " + output[1] + "\n\n");
+                            break;
+                    }
+
+                }
+
+                output = getSalesInfo(storeId, 2019);
+                s.Append("2019 Yearly Orders: " + output[0] + "\n" +
+                         "2019 Yearly Sales:  " + output[1]);
+
+                MessageBox.Show(s.ToString());
             }
             else
             {
