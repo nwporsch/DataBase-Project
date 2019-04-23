ea65f900e244e30880f0ed8e6c3a443b21350e8b
apjavier <apjavier@ksu.edu>
2019-04-23 18:00:23 -0500

    Finished version of GetSalesInfo and Storeview?  I'm not sure if they work or how to test them.

--- Game Project Application/Game Project Application/SQL/Procedures/GetSalesInfo.sql
+++ Game Project Application/Game Project Application/SQL/Procedures/GetSalesInfo.sql
@@ -3,7 +3,7 @@
 	@StoreId INT
 AS
 
-WITH OrderTotalCte(StoreID, OrderYear, OrderMonth, Sales, OrderCount) AS
+WITH SalesTotalCte(StoreID, OrderYear, OrderMonth, Sales, OrderCount) AS
 	(
 		SELECT S.StoreId,
 			YEAR(O.OrderDate) OrderYear,
@@ -13,18 +13,15 @@ WITH OrderTotalCte(StoreID, OrderYear, OrderMonth, Sales, OrderCount) AS
 		FROM GameStore.Orders O
 			INNER JOIN GameStore.OrderLines OL ON OL.OrderId = O.OrderId
 			INNER JOIN GameStore.Stores S ON S.StoreId = @StoreId
+		WHERE S.StoreId = @StoreId AND (MONTH(OT.OrderMonth) = @Month OR YEAR(OT.OrderYear) = @Month)
 		GROUP BY S.StoreId, MONTH(O.OrderDate), YEAR(O.OrderDate)
 	)
 SELECT S.StoreId, OT.OrderYear, OT.OrderMonth, OT.Sales,
+	OT.OrderCount,
 	SUM(OT.Sales) OVER(
-		PARTITION BY OT.StoreId, OT.OrderYear
+		PARTITION BY OT.StoreId
 		ORDER BY OT.StoreId ASC
-		ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS YtdTotal,
-	SUM(OT.Sales) OVER(
-		PARTITION BY OT.StoreId, OT.OrderMonth
-		ORDER BY OT.StoreId ASC
-		ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS MonthTotal
-FROM OrderTotalCte OT
+		ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS Total
+FROM SalesTotalCte OT
 	INNER JOIN GameStore.Stores S ON S.StoreId = SC.StoreId
-WHERE S.StoreId = @StoreId
 ORDER BY S.StoreId, OT.OrderYear ASC, OT.OrderMonth ASC
