62707b6a81c2f6311e252946f987290601a21f8e
Nick Porsch <nwporsch@ksu.edu>
2019-04-24 09:21:46 -0500

    DOING MORE DEBUGGING

---
+++ Game Project Application/Game Project Application/SalesInformation.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Game_Project_Application
+{
+    partial class SalesInformation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxSalesText = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // uxSalesText
+            // 
+            this.uxSalesText.Location = new System.Drawing.Point(13, 13);
+            this.uxSalesText.Multiline = true;
+            this.uxSalesText.Name = "uxSalesText";
+            this.uxSalesText.ReadOnly = true;
+            this.uxSalesText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.uxSalesText.Size = new System.Drawing.Size(785, 425);
+            this.uxSalesText.TabIndex = 0;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOk.Location = new System.Drawing.Point(13, 456);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(785, 49);
+            this.btnOk.TabIndex = 1;
+            this.btnOk.Text = "Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // SalesInformation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 517);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.uxSalesText);
+            this.Name = "SalesInformation";
+            this.Text = "SalesInformation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox uxSalesText;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
---
+++ Game Project Application/Game Project Application/SalesInformation.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Game_Project_Application
+{
+    public partial class SalesInformation : Form
+    {
+        private int storeId;
+
+        public SalesInformation(int storeId)
+        {
+            this.storeId = storeId;
+            InitializeComponent();
+            GetSales();
+        }
+
+        /// <summary>
+        /// Returns the total number of orders made and total sales of each month and each year
+        /// </summary>
+        /// <param name="storeId"></param>
+        /// <returns></returns>
+        private string[] getSalesInfo(int storeId, int i)
+        {
+            string connectionString = "Server=mssql.cs.ksu.edu;Database=cis560_team21; Integrated Security=true";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var command = new SqlCommand("GameStore.GetSalesInfo", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.Clear();
+                    command.Parameters.AddWithValue("StoreId", storeId);
+                    connection.Open();
+
+                    var k = command.ExecuteReader();
+
+                    string orders = "";
+                    string totalSales = "";
+
+                    while (k.Read())
+                    {
+                        if (k.GetInt32(k.GetOrdinal("OrderMonth")).Equals(i))
+                        {
+                            orders = k.GetInt32(k.GetOrdinal("OrderCount")).ToString();
+                            totalSales = k.GetDecimal(k.GetOrdinal("Sales")).ToString();
+                        }
+                        else if (i == 0 && k.GetInt32(k.GetOrdinal("OrderMonth")).Equals(12))
+                        {
+                            orders = k.GetInt32(k.GetOrdinal("YearlyOrders")).ToString();
+                            totalSales = k.GetDecimal(k.GetOrdinal("YearlySales")).ToString();
+                        }
+                    }
+
+                    k.Close();
+
+                    string[] output = { orders, totalSales };
+                    return output;
+                }
+            }
+        }
+
+
+        private void GetSales()
+        {
+            StringBuilder s = new StringBuilder();
+
+            string[] output;
+
+            for (int i = 1; i <= 12; i++)
+            {
+                output = getSalesInfo(storeId, i);
+                switch (i)
+                {
+                    case 1:
+                        s.Append("Month: January\r\n" +
+                                 "Monthly Orders: " + output[0] + "\r\n" +
+                                 "Monthly Sales:  " + output[1] + "\r\n");
+                        break;
+                    case 2:
+                        s.Append("Month: February\r\n" +
+                                 "Monthly Orders: " + output[0] + "\r\n" +
+                                 "Monthly Sales:  " + output[1] + "\r\n");
+                        break;
+                    case 3:
+                        s.Append("Month: March\r\n" +
+                                 "Monthly Orders: " + output[0] + "\r\n" +
+                                 "Monthly Sales:  " + output[1] + "\r\n");
+                        break;
+                    case 4:
+                        s.Append("Month: April\r\n" +
+                                 "Monthly Orders: " + output[0] + "\r\n" +
+                                 "Monthly Sales:  " + output[1] + "\r\n");
+                        break;
+                    case 5:
+                        s.Append("Month: May\r\n" +
+                                 "Monthly Orders: " + output[0] + "\r\n" +
+                                 "Monthly Sales:  " + output[1] + "\r\n");
+                        break;
+                    case 6:
+                        s.Append("Month: June\r\n" +
+                                 "Monthly Orders: " + output[0] + "\r\n" +
+                                 "Monthly Sales:  " + output[1] + "\r\n");
+                        break;
+                    case 7:
+                        s.Append("Month: July\r\n" +
+                                 "Monthly Orders: " + output[0] + "\r\n" +
+                                 "Monthly Sales:  " + output[1] + "\r\n");
+                        break;
+                    case 8:
+                        s.Append("Month: August\r\n" +
+                                 "Monthly Orders: " + output[0] + "\r\n" +
+                                 "Monthly Sales:  " + output[1] + "\r\n");
+                        break;
+                    case 9:
+                        s.Append("Month: September\r\n" +
+                                 "Monthly Orders: " + output[0] + "\r\n" +
+                                 "Monthly Sales:  " + output[1] + "\r\n");
+                        break;
+                    case 10:
+                        s.Append("Month: October\r\n" +
+                                 "Monthly Orders: " + output[0] + "\r\n" +
+                                 "Monthly Sales:  " + output[1] + "\r\n");
+                        break;
+                    case 11:
+                        s.Append("Month: November\r\n" +
+                                 "Monthly Orders: " + output[0] + "\r\n" +
+                                 "Monthly Sales:  " + output[1] + "\r\n");
+                        break;
+                    case 12:
+                        s.Append("Month: December\r\n" +
+                                 "Monthly Orders: " + output[0] + "\r\n" +
+                                 "Monthly Sales:  " + output[1] + "\r\n\r\n");
+                        break;
+                }
+
+            }
+
+            output = getSalesInfo(storeId, 0);
+            s.Append("Yearly Orders: " + output[0] + "\r\n" +
+                     "Yearly Sales:  " + output[1]);
+
+            this.uxSalesText.Text = s.ToString();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+
+}
