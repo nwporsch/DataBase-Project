2fe07c4e8c5046360765d9baa161b2e512dacd52
schilmjr <schilmjr@ksu.edu>
2019-04-23 20:13:30 -0500

    Changed GetSalesInfo

--- Game Project Application/Game Project Application/SQL/Procedures/GetSalesInfo.sql
+++ Game Project Application/Game Project Application/SQL/Procedures/GetSalesInfo.sql
@@ -6,15 +6,15 @@ AS
 WITH SalesTotalCte(StoreID, OrderYear, OrderMonth, Sales, OrderCount) AS
 	(
 		SELECT S.StoreId,
-			YEAR(O.OrderDate) OrderYear,
-			MONTH(O.OrderDate) OrderMonth,
+			YEAR(O.DatePlaced) OrderYear,
+			MONTH(O.DatePlaced) OrderMonth,
 			SUM(OL.Quantity * OL.UnitPrice) Sales,
 			COUNT(O.OrderId)
 		FROM GameStore.Orders O
 			INNER JOIN GameStore.OrderLines OL ON OL.OrderId = O.OrderId
 			INNER JOIN GameStore.Stores S ON S.StoreId = @StoreId
-		WHERE S.StoreId = @StoreId AND (MONTH(OT.OrderMonth) = @Month OR YEAR(OT.OrderYear) = @Month)
-		GROUP BY S.StoreId, MONTH(O.OrderDate), YEAR(O.OrderDate)
+		WHERE S.StoreId = @StoreId AND (MONTH(O.DatePlaced) = @Month OR YEAR(O.DatePlaced) = @Month)
+		GROUP BY S.StoreId, MONTH(O.DatePlaced), YEAR(O.DatePlaced)
 	)
 SELECT S.StoreId, OT.OrderYear, OT.OrderMonth, OT.Sales,
 	OT.OrderCount,
@@ -23,5 +23,5 @@ SELECT S.StoreId, OT.OrderYear, OT.OrderMonth, OT.Sales,
 		ORDER BY OT.StoreId ASC
 		ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS Total
 FROM SalesTotalCte OT
-	INNER JOIN GameStore.Stores S ON S.StoreId = SC.StoreId
+	INNER JOIN GameStore.Stores S ON S.StoreId = OT.StoreId
 ORDER BY S.StoreId, OT.OrderYear ASC, OT.OrderMonth ASC
