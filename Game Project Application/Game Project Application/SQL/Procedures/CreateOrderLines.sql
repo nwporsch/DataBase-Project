a1315e43dd5120d930ad3ef06610245f2f131701
Nick Porsch <nwporsch@ksu.edu>
2019-04-23 23:18:24 -0500

    Updated CreateOrderLines

--- Game Project Application/Game Project Application/SQL/Procedures/CreateOrderLines.sql
+++ Game Project Application/Game Project Application/SQL/Procedures/CreateOrderLines.sql
@@ -1,4 +1,5 @@
-﻿CREATE OR ALTER PROCEDURE GameStore.CreateOrderLines
+﻿
+CREATE OR ALTER PROCEDURE GameStore.CreateOrderLines
 	@OrderId INT,
 	@OrderLineId INT,
 	@GameStoreInfoId INT,
@@ -6,21 +7,22 @@
 	@UnitPrice DECIMAL(4,2)
 AS
 
-WITH SourceCTE (OrderId,OrderLineId, GameStoreInfoId, Quantity, UnitPrice) AS
+WITH SourceCTE (OrderId, GameStoreInfoId, Quantity, UnitPrice) AS
 (
-	SELECT V.OrderId, V.OrderLineId, V.GameStoreInfoId, V.Quantity, V.UnitPrice 
+
+	SELECT V.OrderId, V.GameStoreInfoId, V.Quantity, V.UnitPrice 
 	FROM 
 	(
-		VALUES (@OrderId,@OrderLineId,@GameStoreInfoId,@Quantity,@UnitPrice)
-	) V (OrderId,OrderLineId,GameStoreInfoId,Quantity,UnitPrice)
+		VALUES (@OrderId,@GameStoreInfoId,@Quantity,@UnitPrice)
+	) V (OrderId,GameStoreInfoId,Quantity,UnitPrice)
 ) 
 
 MERGE GameStore.OrderLines OL
-USING SourceCTE S ON S.OrderLineId = OL.OrderLineId AND S.OrderId = OL.OrderId
+USING SourceCTE S ON S.OrderId = OL.OrderId AND OL.GameStoreInfoId = S.GameStoreInfoId
 WHEN MATCHED THEN
 	UPDATE
 	SET
 		Quantity = OL.Quantity + S.Quantity
-WHEN NOT MATCHED AND EXISTS (SELECT * FROM GameStore.Orders O WHERE O.OrderId = S.OrderId) THEN
+WHEN NOT MATCHED AND EXISTS (SELECT * FROM GameStore.OrderLines L WHERE L.OrderId = S.OrderId) THEN
 	INSERT (OrderId,GameStoreInfoId,UnitPrice,Quantity)
 		VALUES(@OrderId,@GameStoreInfoId,@UnitPrice,@Quantity);
