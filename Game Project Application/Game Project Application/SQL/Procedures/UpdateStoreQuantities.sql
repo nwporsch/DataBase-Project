852014cc65edd82b4c5f2b822cf9d76b2115fa97
Nick Porsch <porschnickalas@gmail.com>
2019-04-24 10:13:20 -0500

    Forgot to change it in VS

--- Game Project Application/Game Project Application/SQL/Procedures/UpdateStoreQuantities.sql
+++ Game Project Application/Game Project Application/SQL/Procedures/UpdateStoreQuantities.sql
@@ -1,30 +1,22 @@
-﻿CREATE OR ALTER PROCEDURE GameStore.GetSalesInfo
-	@StoreId INT
+﻿R ALTER PROCEDURE GameStore.UpdateStoreQuantities
+	@OrderLineId INT,
+	@GameStoreInfoId INT,
+	@Quantity INT
 AS
 
-WITH SalesTotalCte(StoreID, OrderYear, OrderMonth, Sales, OrderCount) AS
+WITH SourceCTE (OrderLineId, GameStoreInfoId, Quantity) AS
+(
+
+	SELECT V.OrderLineId, V.GameStoreInfoId, V.Quantity
+	FROM 
 	(
-		SELECT S.StoreId,
-			YEAR(O.DatePlaced) OrderYear,
-			MONTH(O.DatePlaced) OrderMonth,
-			SUM(OL.Quantity * OL.UnitPrice) Sales,
-			COUNT(O.OrderId)
-		FROM GameStore.Orders O
-			INNER JOIN GameStore.OrderLines OL ON OL.OrderId = O.OrderId
-			INNER JOIN GameStore.Stores S ON S.StoreId = @StoreId
-		WHERE S.StoreId = @StoreId
-		GROUP BY S.StoreId, MONTH(O.DatePlaced), YEAR(O.DatePlaced)
-	)
-SELECT S.StoreId, OT.OrderYear, OT.OrderMonth, OT.Sales,
-	OT.OrderCount,
-	SUM(OT.Sales) OVER(
-		PARTITION BY OT.StoreId, OT.OrderYear
-		ORDER BY OT.StoreId ASC
-		ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS YearlySales,
-	SUM(OT.OrderCount) OVER(
-		PARTITION BY OT.StoreID, OT.OrderYear
-		ORDER BY OT.StoreId ASC
-		ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS YearlyOrders
-FROM SalesTotalCte OT
-	INNER JOIN GameStore.Stores S ON S.StoreId = OT.StoreId
-ORDER BY S.StoreId, OT.OrderYear ASC, OT.OrderMonth ASC
+		VALUES (@OrderLineId,@GameStoreInfoId,@Quantity)
+	) V (OrderLineId,GameStoreInfoId,Quantity)
+) 
+
+MERGE GameStore.GameStoreInfo GSI
+USING SourceCTE S ON GSI.GameStoreInfoId = S.GameStoreInfoId AND EXISTS (SELECT * FROM GameStore.OrderLines OL WHERE OL.OrderLineId = S.OrderLineId)
+WHEN MATCHED THEN
+	UPDATE
+	SET
+		Quantity = GSI.Quantity - S.Quantity;
